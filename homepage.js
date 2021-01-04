342501c1b7d72dcbffee5db0181afdebedf1743a
sunmuffin42 <sunkulp.ananth@gmail.com>
2021-01-04 13:30:34 -0600

    Remove ignored files

--- homepage.js
+++ homepage.js
@@ -1,11 +1,10 @@
-// window.open("./drankthings.html")
-
 // shoot for 10/10/10 | 15 | 10/10/10
+// that means 40/15 (55)
 // 1min = 60,000
 // 10 min = 600,000
 // 15 min = 900,000
 
-let thingsthings = ["./drankthings.html", "./awakenthings.html", "./posturethings.html"]
+let thingsthings = ["./hydrate.html", "./center.html", "./posture.html"]
 let audio = new Audio("./paging_sound.mp3")
 let traverse = 0
 let interval = setInterval(() => {
@@ -13,7 +12,7 @@ let interval = setInterval(() => {
     audio.play()
     traverse += 1
     if (traverse % 3 == 1 & traverse > 1) {
-        window.open("./chillthings.html")
+        window.open("./chill.html")
     }
 }, 600000);
 // }, 10000); (for testing; 10 secs)
